d4390b9fba289caf25c62be3f5cac59a55c2c139
Holden Omans <holden.omans@gmail.com>
2020-08-17 17:55:24 -0400

    fix bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1110,7 +1110,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 210
-#define Y_BED_SIZE 230
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -10
